82391951b67fd433abdcf1b8db0c39f0fb5e6e1a
DonggunJung <donggun.dev@gmail.com>
2019-10-09 15:07:59 +0900

    Setting API

--- src/target/API/index.js
+++ src/target/API/index.js
@@ -1,1 +1,9 @@
-export const API_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+import config from 'config'
+
+const key = config.get('Customer.API_KEY')
+
+const query = `?key=${key}`
+
+const API_URL = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json${query}`
+
+export default API_URL
