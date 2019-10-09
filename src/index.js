d0be3b709586f0aa97c9e42d3c709f380f1c3a32
DonggunJung <donggun.dev@gmail.com>
2019-10-09 12:34:26 +0900

    Connect Database in sequelize

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import database from './models/model'
+
+database.sequelize.sync().then(() => {
+  console.log("✅ DataBase connection Success.")
+}).catch(err => {
+  console.log("🚨 DataBase connection Failed.")
+})
---
+++ src/models/model.js
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize'
+import { get } from 'config'
+
+const dbConfig = get('Customer.dbConfig')
+const { database, userName, password, host, dialect } = dbConfig
+
+const sequelize = new Sequelize(database, userName, password, {
+  host,
+  dialect,
+  pool: {
+    max: 100,
+    min: 0,
+    idle: 10000,
+  },
+  define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+  },
+})
+
+export default {
+  sequelize
+}
